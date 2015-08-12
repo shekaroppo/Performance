f2032692915e96db250f574a56da330a3d6dc770
Shekar <shekar.shine@gmail.com>
2015-08-12 15:53:02 +0530

    Added Leak Canary

---
+++ app/src/main/java/com/example/development/performance/leakcanary/BaseApplication.java
@@ -0,0 +1,36 @@
+package com.example.development.performance.leakcanary;
+
+import android.content.Context;
+import android.os.StrictMode;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.GINGERBREAD;
+
+public class BaseApplication extends android.app.Application {
+
+    public static RefWatcher getRefWatcher(Context context) {
+        BaseApplication application = (BaseApplication) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
+    private RefWatcher refWatcher;
+
+  @Override public void onCreate() {
+    super.onCreate();
+    enabledStrictMode();
+      refWatcher = LeakCanary.install(this);
+  }
+
+  private void enabledStrictMode() {
+    if (SDK_INT >= GINGERBREAD) {
+      StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder() //
+          .detectAll() //
+          .penaltyLog() //
+          .penaltyDeath() //
+          .build());
+    }
+  }
+}
---
+++ app/src/main/java/com/example/development/performance/leakcanary/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.development.performance.leakcanary;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+
+import com.example.development.performance.R;
+import com.squareup.leakcanary.RefWatcher;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RefWatcher refWatcher = BaseApplication.getRefWatcher(this);
+        refWatcher.watch(this);
+        View button = findViewById(R.id.asyncTtask);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAsyncTask();
+            }
+        });
+    }
+
+    private void startAsyncTask() {
+        // This async task is an anonymous class and therefore has a hidden reference to the outer
+        // class MainActivity. If the activity gets destroyed before the task finishes (e.g. rotation),
+        // the activity instance will leak.
+        new AsyncTask<Void, Void, Void>() {
+            @Override protected Void doInBackground(Void... params) {
+                // Do some slow work in background
+                SystemClock.sleep(20000);
+                return null;
+            }
+        }.execute();
+    }
+}
